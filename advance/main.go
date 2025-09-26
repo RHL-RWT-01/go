13938e3a2099258f699375b2a3c8fd9ff39abca2
Rahul Rawat <rahulrawat4060@gmail.com>
2025-09-26 23:27:25 +0530

    implement goroutine for sending messages and simplify code structure

--- advance/main.go
+++ advance/main.go
@@ -1,35 +1,50 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	// "time"
-)
-
-// func worker(ch chan string) {
-// 	ch <- "Task completed!"
-// }
-
-func Hi(i int) {
-	fmt.Println("Hi", i)
-}
+import "fmt"
 
 func main() {
+	ch := make(chan string)
 
-	var wg sync.WaitGroup
+	// goroutine sending
+	go func() {
+		ch <- "Hello from goroutine"
+	}()
 
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			Hi(i)
-		}(i)
-	}
-
-	wg.Wait()
-	// time.Sleep(time.Second * 1)
-	// messageChannel := make(chan string)
-	// go worker(messageChannel)
-	// msg := <-messageChannel
-	// fmt.Println(msg)
+	// receiving
+	msg := <-ch
+	fmt.Println(msg)
 }
+
+// import (
+// 	"fmt"
+// 	"sync"
+// 	// "time"
+// )
+
+// // func worker(ch chan string) {
+// // 	ch <- "Task completed!"
+// // }
+
+// func Hi(i int) {
+// 	fmt.Println("Hi", i)
+// }
+
+// func main() {
+
+// 	var wg sync.WaitGroup
+
+// 	for i := 0; i < 10; i++ {
+// 		wg.Add(1)
+// 		go func(i int) {
+// 			defer wg.Done()
+// 			Hi(i)
+// 		}(i)
+// 	}
+
+// 	wg.Wait()
+// 	// time.Sleep(time.Second * 1)
+// 	// messageChannel := make(chan string)
+// 	// go worker(messageChannel)
+// 	// msg := <-messageChannel
+// 	// fmt.Println(msg)
+// }
